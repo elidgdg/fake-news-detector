e0591d397f6bef480587f98f9397afd8a35cfd56
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2025-07-24 17:33:46 +0100

    Add training and evaluation to main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from data_loader import load_data
 from preprocessing import preprocess
+from train import train_model
+from evaluate import evaluate_model
 
 df = load_data()
 X_train, X_test, y_train, y_test = preprocess(df)
+model, vectorizer = train_model(X_train, y_train)
+evaluate_model(model, vectorizer, X_test, y_test) 
