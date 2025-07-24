0981b2d86231f4e34532d3b630fbf98128de0274
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2025-07-24 17:26:30 +0100

    Create training function

---
+++ train.py
@@ -0,0 +1,13 @@
+from sklearn.feature_extraction.tests import TfidVectorizer
+from sklearn.linear_model import LogisticRegression
+
+def train_model(X_train, y_train):
+    # Convert text to TF-IDF vectors
+    vectorizer = TfidVectorizer(max_df=0.7)
+    X_train_vec = vectorizer.fit_transform(X_train)
+
+    # Train logistic regression
+    model = LogisticRegression()
+    model.fit(X_train_vec, y_train)
+
+    return model, vectorizer
