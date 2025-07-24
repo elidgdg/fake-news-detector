986b8652c48305ec7b3bcebb362a2b47ee70057d
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2025-07-24 17:48:43 +0100

    app code

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from src.preprocessing import clean_text
+import pickle
+
+# Load model and vectorizer
+with open("models/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+with open("models/vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        text = request.form["news"]
+        cleaned = clean_text(text)
+        vec = vectorizer.transform([cleaned])
+        pred = model.predict(vec)[0]
+        prediction = "Real News" if pred == 1 else "Fake News"
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
