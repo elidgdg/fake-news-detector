0bfeacc164cfcc02f0b41b41bb750afb3ccf5aac
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2025-07-24 17:51:21 +0100

    Add submit button

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from flask import Flask, render_template, request
 from src.preprocessing import clean_text
 import pickle
